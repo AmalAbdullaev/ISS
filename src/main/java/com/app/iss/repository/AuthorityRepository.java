01f7bf1685d3e48c537ee27266e7efdbc0a73e3e
User <user@yandex.ru>
2022-03-08 17:27:50 +0300

    Initial application generated by JHipster-6.0.0

---
+++ src/main/java/com/app/iss/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.app.iss.repository;
+
+import com.app.iss.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
